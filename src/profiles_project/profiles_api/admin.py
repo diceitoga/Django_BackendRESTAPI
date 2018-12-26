d9495fbf179455f0880843f845ac7b03f3efab82
Daisuke Itoga <diceitoga@hotmail.com>
2018-12-26 06:59:37 -0800

    Enabled the Django admin for the user profile model

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register(models.UserProfile)
+'''
+In the django.contrib, there is admin module that we importedself.
+In that module is a site module which has a prebuilt function called
+register, in which you pass the model you want to registerself.
+In this particular case, it is models==models object and UserProfile,
+which you defined in models.py
+'''
